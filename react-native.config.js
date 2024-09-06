f93abd081950de1816f24068d0edfe1bd58b232c
toan.ngo <toan.ngo@southtelecom.vn>
2024-09-06 15:30:06 +0700

    get it work

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-    dependency: {
-        platforms: {
-            ios: {
-                podspecPath: require.resolve('react-native-audio-speaker/react-native-audio-speaker.podspec'),
+    dependencies: {
+        "react-native-audio-speaker": {
+            platforms: {
+                ios: {
+                    podspecPath: require.resolve('react-native-audio-speaker/react-native-audio-speaker.podspec'),
+                },
+                android: {
+                    sourceDir: '../node_modules/react-native-audio-speaker/android',
+                },
             },
-            android: {
-                sourceDir: '../node_modules/react-native-audio-speaker/android',
-            },
-        },
+        }
     },
 };
