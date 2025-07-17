2c2cf29653c962e7fba653940080c3b3cf577032
toan.ngo <toan.ngo@southtelecom.vn>
2025-07-17 10:28:32 +0700

    update module

--- src/NativeAudioSpeaker.ts
+++ src/NativeAudioSpeaker.ts
@@ -3,10 +3,12 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-    toggleSpeaker(): void;
-    isSpeaker(): Promise<boolean | 1 | 0>;
-    switchToReceiver(): void;
-    switchToSpeaker(): void;
+  toggleSpeaker(): void;
+  isSpeaker(): Promise<boolean | 1 | 0>;
+  switchToReceiver(): void;
+  switchToSpeaker(): void;
 }
 
-export default TurboModuleRegistry.getEnforcing<Spec>('RNAudioSpeaker');
+const RNAudioSpeaker = TurboModuleRegistry.get<Spec>('RNAudioSpeaker');
+
+export default RNAudioSpeaker ?? null;
